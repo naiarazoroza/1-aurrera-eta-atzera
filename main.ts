42c8fc7e799b627f44dd2253b72f26c37db9af6b
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2022-03-16 14:27:06 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+basic.pause(2000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
+basic.pause(500)
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 30)
+basic.pause(5000)
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
+basic.forever(function () {
+	
+})
